e287f4c0414e3ea177ddce58e78b2a05b59edbf1
Ezequiel Luberriaga <eze@Ezequiels-MacBook-Air.local>
2026-06-26 20:30:10 -0300

    perf: split vendor chunks to reduce main bundle size

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom'],
+          'vendor-charts': ['recharts'],
+          'vendor-xlsx': ['xlsx'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600,
+  },
 })
